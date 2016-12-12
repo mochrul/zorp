a1ee8f2d604c3d99ff71a39ccc3dee1d9d13924d
SZALAY Attila <sasa@ubainba.hu>
2016-12-12 22:14:40 +0100

    Adapt test policy file to the new version

--- debian/tests/policies/basic.py
+++ debian/tests/policies/basic.py
@@ -2,18 +2,14 @@ from Zorp.Core import *
 from Zorp.Plug import *
 from Zorp.Http import *
 from Zorp.Ftp import *
-
+from Zorp.Dispatch import *
 
 		
-InetZone("local", "127.0.0.0/8",
-         inbound_services=["*"],
-         outbound_services=["*"])
+InetZone("local", "127.0.0.0/8")
         
-InetZone("internet", "0.0.0.0/0",
-         inbound_services=[],
-         outbound_services=[])
+InetZone("internet", "0.0.0.0/0")
 
 
 def zorp():
     Service("test", PlugProxy, router=DirectedRouter(SockAddrInet("127.0.0.2", 50081)))
-    Listener(SockAddrInet("127.0.0.2", 50080), "test")
+    Dispatcher(bindto=convertSockAddrToDB(SockAddrInet("127.0.0.2", 50080), ZD_PROTO_TCP), service="test")
